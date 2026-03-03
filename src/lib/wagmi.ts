2a4043c45bc53ccad84b34516f4548f48bf567fc
orsinibear <udochukwugodwin13@gmail.com>
2026-03-03 05:39:43 +0300

    push from windows set-up to pop

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,17 +1,14 @@
 import { http, createConfig } from "wagmi";
-import { mainnet, worldchain } from "wagmi/chains";
+import { worldchain, sepolia } from "wagmi/chains";
 import { injected, walletConnect } from "wagmi/connectors";
 
 const projectId = "b5e20d234e9814e32ce0b81b31a77499"; // WalletConnect Cloud project ID
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet, worldchain],
-  connectors: [
-    injected(),
-    walletConnect({ projectId }),
-  ],
+  chains: [sepolia, worldchain],
+  connectors: [injected(), walletConnect({ projectId })],
   transports: {
-    [mainnet.id]: http(),
+    [sepolia.id]: http(),
     [worldchain.id]: http(),
   },
 });
